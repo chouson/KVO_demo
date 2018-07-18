0ebab45735788c1a6d9ff26ae50c5099c4250a65
陈朝政 <chouson_chan@chouson-chandemacbook-air.local>
2018-07-18 16:42:43 +0800

    Initial Commit

---
+++ KVO_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO_demo
+//
+//  Created by Chouson_Chan on 2018/7/18.
+//  Copyright © 2018年 Chouson_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO_demo
+//
+//  Created by Chouson_Chan on 2018/7/18.
+//  Copyright © 2018年 Chouson_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
